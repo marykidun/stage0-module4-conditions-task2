23a8a2a9a2b3a73f24eecf8d13680618f29a5ecf
Maryna Kidun <marykidun@gmail.com>
2022-10-25 22:56:30 +0300

    Implemented two tasks

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,17 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void printGreatest(int first, int second) {
+        if (first == second)
+            System.out.println(first | second);
+        else if (first > second)
+            System.out.println(first);
+        else
+            System.out.println(second);
+
+    }
+
+    public static void main(String[] args) {
+        printGreatest(10, 12);
     }
 }
